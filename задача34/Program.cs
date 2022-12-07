836d431f2a7ea2018a66f8c82fa4be8947bf994b
mamonchikmarina <mamonchik.marina@yandex.ru>
2022-12-07 22:43:13 +0300

    HW5

---
+++ задача34/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными 
+трёхзначными числами. Напишите программу, которая покажет количество 
+чётных чисел в массиве.
+[345, 897, 568, 234] -> 2 */
+
+
+//инициализирует новый массив
+int[] InitArray(int length)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(0,1000);
+    }
+    return array;
+}
+
+//печатает массив на экран
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+//находим четные числа
+int Count(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 == 0) count++;
+    }
+    return count;
+}
+
+int[] array = InitArray(12);
+PrintArray(array);
+
+Console.WriteLine($"количество чётных чисел: {Count(array)}");
